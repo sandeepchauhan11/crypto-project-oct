7b76e6a88622a085ecacbe552b1f671fe990c3a8
sandeepchauhan11 <sandeepchauhan71999@gmail.com>
2024-10-06 01:17:48 +0530

    Updates

---
+++ precache-manifest.adc54a7afa319d75178b9d5339ba894a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "377f4a019eeec6391cde25bda775ae78",
+    "url": "/sandeepchauhan11/crypto-project-oct.git/index.html"
+  },
+  {
+    "revision": "7762d04c2b5173214b85",
+    "url": "/sandeepchauhan11/crypto-project-oct.git/static/css/main.e7f1fd8a.chunk.css"
+  },
+  {
+    "revision": "f18e9280047d2c2f67b5",
+    "url": "/sandeepchauhan11/crypto-project-oct.git/static/js/2.afef58d2.chunk.js"
+  },
+  {
+    "revision": "7762d04c2b5173214b85",
+    "url": "/sandeepchauhan11/crypto-project-oct.git/static/js/main.41078f3c.chunk.js"
+  },
+  {
+    "revision": "523ccd5818c4a4ddddf2",
+    "url": "/sandeepchauhan11/crypto-project-oct.git/static/js/runtime~main.4dabfd00.js"
+  }
+]);
